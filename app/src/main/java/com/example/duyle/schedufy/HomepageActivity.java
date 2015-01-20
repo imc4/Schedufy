f1fe4fabfbb469f103fe95bdc49eaa791b556f41
Ig M <igsmar@gmail.com>
2015-01-20 09:02:41 -0800

    Initial commit

---
+++ app/src/main/java/com/example/duyle/schedufy/HomepageActivity.java
@@ -0,0 +1,14 @@
+package com.example.duyle.schedufy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomepageActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_homepage);
+    }
+}
---
+++ app/src/main/java/com/example/duyle/schedufy/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.duyle.schedufy;
+
+import android.app.TabActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TabHost;
+import android.content.Intent;
+
+
+public class MainActivity extends TabActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TabHost mTabHost = getTabHost();
+
+        mTabHost.addTab(mTabHost.newTabSpec("first").setIndicator("Home").setContent(new Intent(this,HomepageActivity.class)));
+        mTabHost.addTab(mTabHost.newTabSpec("second").setIndicator("Calendar").setContent(new Intent(this,CalendarActivity.class)));
+
+        mTabHost.setCurrentTab(0);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
